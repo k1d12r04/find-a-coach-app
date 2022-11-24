6163afb767a9807d2d1a77d4efc8b3aead07bb1c
Kadir <k1d12r04@gmail.com>
2022-11-24 22:28:58 +0300

    Make register form as a coach

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,16 @@
-<template>REGISTER AS COACH</template>
+<template>
+  <base-card>
+    <h2>Register as a coach now!</h2>
+    <coach-form></coach-form>
+  </base-card>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
